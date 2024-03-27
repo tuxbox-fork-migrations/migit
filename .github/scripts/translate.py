6a7ba9da36303e1c2967914c05deb40e719284fe
Thilo Graf <dbt@novatux.de>
2024-03-27 19:15:11 +0100

    Readme: add global readme with linked to localized readme files

--- .github/scripts/translate.py
+++ .github/scripts/translate.py
@@ -6,12 +6,17 @@ from googletrans import Translator
 def translate_readme(input_text, target_lang):
     translator = Translator()
     translated = translator.translate(input_text, dest=target_lang)
+    translated_text = translated.text
+
+    # add hint for automatically translation
+    translated_text = f"Note: This is an automatically translated file. Original content from [here](https://github.com/tuxbox-fork-migrations/migit/blob/master/README-de.md):\n\n{translated_text}"
+
     return translated.text
 
 if __name__ == "__main__":
-    input_text = open("README.md", "r").read()
+    input_text = open("README-de.md", "r").read()
     target_lang = "en"  # Ziel-Sprachcode für Englisch
     translated_text = translate_readme(input_text, target_lang)
 
-    with open("README_en.md", "w") as outfile:
+    with open("README-en.md", "w") as outfile:
         outfile.write(translated_text)
